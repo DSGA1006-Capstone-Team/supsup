e8583aca6e9fb630a71d1ed181a8cbe2cf72b047
Amber Teng <at2507@nyu.edu>
2020-10-31 11:34:47 -0400

    Update supsup_slurm_v3.s

--- supsup_slurm_v3.s
+++ supsup_slurm_v3.s
@@ -7,7 +7,7 @@
 #SBATCH --job-name=dsga1006-supsup
 #SBATCH --mail-type=END
 #SBATCH --mail-user=at2507@nyu.edu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --output=slurm_supsup_%j.out
 
 # Refer to https://sites.google.com/a/nyu.edu/nyu-hpc/documentation/prince/batch/submitting-jobs-with-sbatch
@@ -34,7 +34,7 @@ pip install torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pyto
 # python ./experiments/GG/splitcifar100/rn18-supsup.py  --data="./data" --seeds 1
 # python experiments/GG/splitcifar100/rn18-supsup.py --gpu-sets="0|1|2|3" --data=./data --seeds 1
 # python experiments/GG/splitcifar100/rn18-supsup.py --gpu-sets="0|1" --data=./data --seeds 1
-python experiments/SupsupSeed/splitcifar100/rn18-supsup_small.py --data="./data" --seeds 1 --num-masks 3 --gpu-sets="1"
+python experiments/SupsupSeed/splitcifar100/rn18-supsup_small.py --data="./data" --seeds 1 --num-masks 3 --gpu-sets="0|1"
 # python experiments/SupsupSeed/splitcifar100/rn18-supsup.py --data="./data" --seeds 1 --num-masks 5 --gpu-sets="0"
 # python experiments/SupsupSeed/splitcifar100/rn18-supsup.py --data="./data" --seeds 1 --num-masks 7 --gpu-sets="0"
 # python experiments/SupsupSeed/splitcifar100/rn18-supsup.py --data="./data" --seeds 1 --num-masks 10 --gpu-sets="0"
