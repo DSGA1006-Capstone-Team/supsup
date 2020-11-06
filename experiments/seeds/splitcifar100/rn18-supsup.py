f2e29c6d59e82b2802cbf1ed17bcd0521f0f5342
Dhrupad Bhardwaj <db4045@nyu.edu>
2020-11-06 12:04:31 -0500

    Seed + expiments using comming dir conda

--- experiments/seeds/splitcifar100/rn18-supsup.py
+++ experiments/seeds/splitcifar100/rn18-supsup.py
@@ -10,8 +10,8 @@ sys.path.append(os.path.abspath("."))
 
 # note: new algorithm code
 def kwargs_to_cmd(kwargs):
-    #cmd = "/home/db4045/.conda/envs/capstone/bin/python basis.py "
-    cmd = "/scratch/db4045/capstone_env/bin/python basis.py "
+#    cmd = "/home/db4045/.conda/envs/capstone/bin/python main.py "
+    cmd = "/scratch/db4045/capstone_env/bin/python main.py "
     for flag, val in kwargs.items():
         cmd += f"--{flag}={val} "
 
@@ -41,19 +41,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu-sets', default=0, type=lambda x: [a for a in x.split("|") if a])
     parser.add_argument('--seeds', default=1, type=int)
-    parser.add_argument('--data', default='/scratch/db4045/data', type=str)
-    parser.add_argument('--seed_model_dir', default='/scratch/db4045/seed_models_{num_masks}/id\=supsup~seed\={seed}~sparsity\={sparsity}~try\=0/', type=str)
+    parser.add_argument('--data', default='/scratch/db404/data', type=str)
     parser.add_argument('--num-masks', default=20, type=int)
-    parser.add_argument('--logdir-prefix', type=str)
-    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--logdir-prefix', type=str, required=True)
     args = parser.parse_args()
 
     gpus = args.gpu_sets
     seeds = list(range(args.seeds))
     data = args.data
 
-    config = "experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml"
-    log_dir = "{scratch}/runs/{logdir_prefix}/SupsupSeedBasis/rn18-supsup_basis_num_masks_{num_masks}".format(num_masks=str(args.num_masks), scratch=os.environ.get("SCRATCH"), logdir_prefix=args.logdir_prefix)
+    config = "experiments/seeds/splitcifar100/configs/rn18-supsup{}.yaml".format("" if args.num_masks == 20 else "_{}".format(str(args.num_masks)))
+    log_dir = "/scratch/{user}/runs/{logdir_prefix}/SupsupSeed/rn18-supsup_num_masks_{num_masks}".format(user=os.environ.get("USER"), num_masks=str(args.num_masks), logdir_prefix=args.logdir_prefix)
     experiments = []
     sparsities = [1, 2, 4, 8, 16, 32] # Higher sparsity values mean more dense subnetworks
 
@@ -61,11 +60,12 @@ def main():
     for sparsity, seed in product(sparsities, seeds):
         kwargs = {
             "config": config,
-            "name": f"id=basis-supsup~seed={seed}~sparsity={sparsity}",
+            "name": f"id=supsup~seed={seed}~sparsity={sparsity}",
+            "sparsity": sparsity,
+            "seed": seed,
             "log-dir": log_dir,
-            "epochs": args.epochs,
-            "data": data,
-            "seed-model": "{}/final.pt".format(args.seed_model_dir.format(sparsity=str(sparsity), seed=str(seed), num_masks=str(args.num_masks)))
+            "epochs": int(args.epochs),
+            "data": data
         }
 
         experiments.append(kwargs)
