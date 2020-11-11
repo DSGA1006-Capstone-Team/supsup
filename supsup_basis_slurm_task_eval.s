2a09323a467eeae7c00915263a3bc51da8eaf452
Dhrupad Bhardwaj <db4045@nyu.edu>
2020-11-10 20:14:24 -0500

    Slurm file to train a supsup model and run inference to test preservation of performance via basis.py

--- supsup_basis_slurm_task_eval.s
+++ supsup_basis_slurm_task_eval.s
@@ -7,7 +7,7 @@
 #SBATCH --job-name=dsga1006-supsup-basis
 #SBATCH --mail-type=END
 #SBATCH --mail-user=db4045@nyu.edu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:k80:1
 #SBATCH --output=slurm_supsup_basis_task_eval_%j.out
 
 # Refer to https://sites.google.com/a/nyu.edu/nyu-hpc/documentation/prince/batch/submitting-jobs-with-sbatch
@@ -30,22 +30,19 @@ SRCDIR=$HOME/git/supsup
 # conda activate supsup
 # conda install -y pytorch==1.5.1 torchvision==0.6.1 -c pytorch
 # pip install torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
-rm -rf /scratch/db4045/runs/dhrupad_seed_epoch10_single/
 
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/main.py --data="/scratch/db4045/data" --seed=0 --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/ --config $SRCDIR/experiments/seeds/splitcifar100/configs/rn18-supsup_5.yaml --multigpu="0" --epochs 10 --multigpu="0" --sparsity 8 --name dhrupad_seed_mask
+#rm -rf /scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/main.py --data="/scratch/db4045/data" --seed=0 --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/ --config $SRCDIR/experiments/seeds/splitcifar100/configs/rn18-supsup_5.yaml --multigpu="0" --epochs 10 --multigpu="0" --sparsity 8 --name dhrupad_seed_mask
 #
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml --multigpu="0" --task-eval 3
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis.yaml --multigpu="0" --task-eval 3
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup.yaml --multigpu="0" --task-eval 3
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml --multigpu="0" --task-eval 3 --name dhrupad_basis_multimask --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupBasis/
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis.yaml --multigpu="0" --task-eval 3 --name dhrupad_basis_singlemask --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupBasis/
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/basis.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup.yaml --multigpu="0" --task-eval 3 --name dhrupad_basis_orig --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupBasis/
 
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/main_cpu.py --data="/scratch/db4045/data" --seed=0 --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/ --config $SRCDIR/experiments/seeds/splitcifar100/configs/rn18-supsup_5.yaml --epochs 10 --sparsity 8 --name dhrupad_seed_mask
+#rm -rf /scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed_cpu/
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/main_cpu.py --data="/scratch/db4045/data" --seed=0 --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed_cpu/ --config $SRCDIR/experiments/seeds/splitcifar100/configs/rn18-supsup_5.yaml --epochs 10 --sparsity 8 --name dhrupad_seed_mask
 #
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml --task-eval 3
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis.yaml --task-eval 3
-#/home/db4045/.conda/envs/capstone/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup.yaml --task-eval 3
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed_cpu/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml --task-eval 3 --name dhrupad_basis_multimask --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupBasis/
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed_cpu/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis.yaml --task-eval 3 --name dhrupad_basis_singlemask --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupBasis/
+#/scratch/db4045/capstone_env/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed_cpu/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup.yaml --task-eval 3 --name dhrupad_basis_orig --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupBasis/
 
-
-/scratch/db4045/capstone_env/bin/python $SRCDIR/main_cpu.py --data="/scratch/db4045/data" --seed=0 --log-dir=/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/ --config $SRCDIR/experiments/seeds/splitcifar100/configs/rn18-supsup_5.yaml --epochs 10 --sparsity 8 --name dhrupad_seed_mask
-
-/scratch/db4045/capstone_env/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml --task-eval 3
-/scratch/db4045/capstone_env/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_seed_epoch10_single/SupsupSeed/dhrupad_seed_mask~try=0/final.pt" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup.yaml --task-eval 3
+/scratch/db4045/capstone_env/bin/python $SRCDIR/basis_cpu.py --data="/scratch/db4045/data" --seed=0 --seed-model="/scratch/db4045/runs/dhrupad_runs/SupsupSeed/rn18-supsup_num_masks_5/id=supsup~seed=0~sparsity=4~try=0/final.py" --config $SRCDIR/experiments/basis/splitcifar100/configs/rn18-supsup-basis-multitask.yaml --task-eval 3 --name dhrupad_basis_multimask --log-dir=/scratch/db4045/runs/dhrupad_test/SupsupBasis/
