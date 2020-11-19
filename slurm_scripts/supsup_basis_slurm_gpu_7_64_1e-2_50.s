c7ca8360c2ccd5d12bcfee79085ccf106e39fdb8
Dhrupad Bhardwaj <db4045@nyu.edu>
2020-11-19 15:14:54 -0500

    Update slurm scripts to more reasonable epochs and hyperparams

--- slurm_scripts/supsup_basis_slurm_gpu_7_64_1e-2_50.s
+++ slurm_scripts/supsup_basis_slurm_gpu_7_64_1e-2_50.s
@@ -8,7 +8,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=db4045@nyu.edu
 #SBATCH --gres=gpu:k80:1
-#SBATCH --output=logs/slurm_supsup_basis_gpu_7_hyperparams_32_1e-3_300_%j.out
+#SBATCH --output=logs/slurm_supsup_basis_gpu_7_hyperparams_64_1e-2_50_%j.out
 
 # Refer to https://sites.google.com/a/nyu.edu/nyu-hpc/documentation/prince/batch/submitting-jobs-with-sbatch
 # for more information about the above options
@@ -21,4 +21,4 @@ module load anaconda3/5.3.1
 # Move into the directory that contains our code
 SRCDIR=$(pwd)
 
-/scratch/db4045/capstone_env/bin/python $SRCDIR/experiments/basis/splitcifar100/rn18-supsup-basis.py --data="/scratch/db4045/data" --seeds=1 --num-masks=7 --seed_model_dir="/scratch/db4045/runs/dhrupad_runs_gpu/SupsupSeed/rn18-supsup_num_masks_{num_masks}/id=supsup~seed={seed}~sparsity={sparsity}~try=0/" --logdir-prefix="dhrupad_runs_gpu_hyperparams_32_1e-3_300" --epochs 300 --gpu-sets="0" --lr 1e-3 --batch-size 32
+/scratch/db4045/capstone_env/bin/python $SRCDIR/experiments/basis/splitcifar100/rn18-supsup-basis.py --data="/scratch/db4045/data" --seeds=1 --num-masks=7 --seed_model_dir="/scratch/db4045/runs/dhrupad_runs_gpu/SupsupSeed/rn18-supsup_num_masks_{num_masks}/id=supsup~seed={seed}~sparsity={sparsity}~try=0/" --logdir-prefix="dhrupad_runs_gpu_hyperparams_64_1e-2_50" --epochs 50 --gpu-sets="0" --lr 1e-2 --batch-size 64
