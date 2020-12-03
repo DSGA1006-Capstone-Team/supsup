817af5632ee967ff663094db409fba546587541c
Dhrupad Bhardwaj <db4045@nyu.edu>
2020-12-02 23:59:25 -0500

    Experiments with new hyperparameters and increased sparsity combinations

--- logparser.py
+++ logparser.py
@@ -23,10 +23,16 @@ def logreader(fname):
 
 def get_epoch_loss(line):
     assert line.startswith('Train Epoch:')
-    parts = line.split(' ')
-    epoch = int(parts[2])
-    loss = float(parts[-1])
-    return epoch, loss
+    parts = line.split('Alphas:')
+    mainline = parts[0].strip()
+    mparts = mainline.split(' ')
+    epoch = int(mparts[2])
+    loss = float(mparts[-1])
+    if len(parts) > 1:
+        alphas = yaml.load(parts[1].strip())
+    else:
+        alphas = {}
+    return epoch, loss, alphas
 
 
 def get_test_accuracy_loss(line):
@@ -60,6 +66,7 @@ def logparser(fname, is_basis=False, num_masks=None):
     if not is_basis and num_masks:
         raise ValueError("Cannot have seed experiement with num masks. Do not set a value for num_masks")
     data = [('ID', 'exp_no', 'seed_val', 'sparsity', 'task', 'epochno', 'train_loss', 'test_loss', 'accuracy', 'num_masks', 'seed_model')]
+    testdata = [('ID', 'exp_no', 'seed_val', 'sparsity', 'task', 'epochno', 'train_loss', 'test_loss', 'accuracy', 'num_masks', 'seed_model')]
     print("Starting parse for: {}".format(fname))
     it = logreader(fname)
     try:
@@ -83,21 +90,33 @@ def logparser(fname, is_basis=False, num_masks=None):
                     tloss = 0.0
                     while not line.startswith('Test set: Average loss:'):
                         assert line.startswith('Train Epoch'), "Line: {}".format(line)
-                        epoch_log, tloss = get_epoch_loss(line)
+                        epoch_log, tloss, alphas = get_epoch_loss(line)
                         assert epoch_log == epoch, "Epochs don't line up for {}, {}, {} \n {}".format(exp, epoch, epoch_log, line)
                         line = next(it)
                     accuracy, loss = get_test_accuracy_loss(line)
                     exp_data.append(summarize_details(expno, details, task, epoch, tloss, loss, accuracy, num_masks))
                     line = next(it)
+            while not line.startswith('Testing '):
+                line = next(it)
+            test_data = []
             while not line.startswith('=> Reading YAML config'):
+                if line.startswith('Testing '):
+                    taskidx = int(line.split(' ')[1].strip(':'))
+                    line = next(it)
+                    accuracy = float(line.split(' ')[-1].strip('(').strip(')').strip('%'))
+                    test_data.append(summarize_details(expno, details, taskidx, -1, -1, -1, accuracy, num_masks))
                 line = next(it)
             data += exp_data
+            testdata += test_data
     except StopIteration as stp:
         pass
-        #print("Reached EOF!")
     it.close()
     data += exp_data
-    return pd.DataFrame(data=data[1:], columns=data[0]) 
+    testdata += test_data
+    testdf = pd.DataFrame(data=testdata[1:], columns=testdata[0])
+    testdf = testdf.drop(['train_loss', 'test_loss', 'epochno'], axis=1)
+    return pd.DataFrame(data=data[1:], columns=data[0]), testdf
+
 
 @click.command()
 @click.option("--log-file", type=str, required=True, help="Full path to log file")
@@ -108,10 +127,13 @@ def run(log_file, output_dir, is_basis, num_masks):
     if not os.path.exists(log_file):
         raise ValueError("File not found!: {}".format(log_file))
 
-    df = logparser(log_file, is_basis, num_masks)
+    df, test = logparser(log_file, is_basis, num_masks)
     outfile = os.path.join(output_dir, '{}_results.csv'.format('_'.join(os.path.basename(log_file).split('_')[:-1])))
+    outfile_test = os.path.join(output_dir, '{}_test.csv'.format('_'.join(os.path.basename(log_file).split('_')[:-1])))
     print("Finished parsing log. Writing to: {}".format(outfile))
     df.to_csv(outfile)
+    print("Writing to: {}".format(outfile_test))
+    test.to_csv(outfile_test)
 
 
 if __name__ == "__main__":
